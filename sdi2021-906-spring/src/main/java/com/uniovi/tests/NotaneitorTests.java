3b6003f6395aa3c4614c2536859059baa4b8bc98
FernandoGiganto <fernandogigantorodriguez@gmail.com>
2021-03-03 20:45:51 +0100

    SDI-2021-906-5.4-Casos Uso VistaHome.

--- sdi2021-906-spring/src/main/java/com/uniovi/tests/NotaneitorTests.java
+++ sdi2021-906-spring/src/main/java/com/uniovi/tests/NotaneitorTests.java
@@ -1,7 +1,5 @@
 package com.uniovi.tests;
 
-import static org.junit.Assert.fail;
-
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -11,14 +9,19 @@ import org.junit.Test;
 import org.junit.runners.MethodSorters;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+
+import com.uniovi.tests.pageobjects.PO_HomeView;
+import com.uniovi.tests.pageobjects.PO_Properties;
+import com.uniovi.tests.util.SeleniumUtils;
+
+
 //Ordenamos las pruebas por el nombre del método 
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 
 public class NotaneitorTests {
 
-	// En Windows (Debe ser la versión 65.0.1 y desactivar las actualizacioens
-	// automáticas)):
-	static String PathFirefox65 = "C:\\Program Files\\Mozilla Firefox\firefox.exe";
+	// En Windows (Debe ser la versión 65.0.1 y desactivar las actualizacioens automáticas)):
+	static String PathFirefox65 = "C:\\Program Files\\Mozilla Firefox\\firefox.exe";
 	static String Geckdriver024 = "C:\\Users\\ferna\\Desktop\\PL-SDI-Sesión5-material\\PL-SDI-Sesión5-material\\geckodriver024win64.exe";
 	// Común a Windows y a MACOSX
 	static WebDriver driver = getDriver(PathFirefox65, Geckdriver024);
@@ -56,8 +59,28 @@ public class NotaneitorTests {
 		driver.quit(); 
 	}
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+	//PR01. Acceder a la página principal / 
+	@Test 
+	public void PR01() { 
+		PO_HomeView.checkWelcome(driver, PO_Properties.getSPANISH()); 
+	}
+	
+	//PR02. OPción de navegación. Pinchar en el enlace Registro en la página home 
+	@Test 
+	public void PR02() { 
+		PO_HomeView.clickOption(driver, "signup", "class", "btn btn-primary");
+	} 
+	
+	//PR03. OPción de navegación. Pinchar en el enlace Identificate en la página home 
+	@Test 
+	public void PR03() { 
+		PO_HomeView.clickOption(driver, "login", "class", "btn btn-primary"); 
+	}
+	
+	//PR04. OPción de navegación. Cambio de idioma de Español a Ingles y vuelta a Español 
+	@Test 
+	public void PR04() { 
+		PO_HomeView.checkChangeIdiom(driver, "btnSpanish", "btnEnglish", PO_Properties.getSPANISH(), PO_Properties.getENGLISH()); 
+		//SeleniumUtils.esperarSegundos(driver, 2);
 	}
 }
